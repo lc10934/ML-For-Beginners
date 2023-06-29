10cb4ee0c2f702a49cef4d41c13eecdf2682b77c
lc10934 <lsc210003@utdallas.edu>
2023-06-28 21:09:18 -0500

    chat bot

---
+++ 6-NLP/1-Introduction-to-NLP/nlpbot.py
@@ -0,0 +1,20 @@
+import random
+
+random_responses = ["That is quite interesting, please tell me more.",
+                    "I see. Do go on.",
+                    "Why do you say that?",
+                    "Funny weather we've been having, isn't it?",
+                    "Let's change the subject.",
+                    "Did you catch the game last night?"]
+
+print("Hello, I am Marvin, the simple robot.")
+print("You can end this conversation at any time by typing 'bye'")
+print("After typing each answer, press 'enter'")
+print("How are you today?")
+while True:
+    user = input("> ")
+    if user== "bye":
+        break
+    response = random.choices(random_responses)
+    print(response)
+print("It was nice talking to you, goodbye!")
